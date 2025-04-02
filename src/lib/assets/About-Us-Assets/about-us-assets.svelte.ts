e936be7acaf2a53d74263daeea1d1fa588166743
Emman Tero <emmantero>
2025-04-02 10:21:30 +0800

    finished about us page

--- src/lib/assets/About-Us-Assets/about-us-assets.svelte.ts
+++ src/lib/assets/About-Us-Assets/about-us-assets.svelte.ts
@@ -6,3 +6,15 @@
 
 import _Header from "./header.png"
 export const Header = _Header
+
+import _CardImg from "./card-img.png"
+export const CardImg = _CardImg
+
+import _ImpactOne from "./Impact One.png"
+export const ImpactOne = _ImpactOne
+
+import _ImpactTwo from "./Impact Two.png"
+export const ImpactTwo = _ImpactTwo
+
+import _Operate from "./howWeOperateImg.png"
+export const Operate = _Operate
--- src/lib/views/AboutPage/AboutPage.svelte
+++ src/lib/views/AboutPage/AboutPage.svelte
@@ -26,4 +26,70 @@
 		</div>
 		<img src={AboutUsAssets.Header} alt="" srcset="" class="aboutUs-page-bg" />
 	</section>
+
+	<section class="aboutUs-summary">
+
+		<div class="whoWeAre">
+			<div class="whoWeAre-title">
+				<h1>Who We Are</h1>
+			</div>
+
+			<div class="whoWeAre-card">
+				<div class="whoWeAre-card-img">
+					<img src={AboutUsAssets.CardImg} alt="" srcset="" class="" />
+				</div>
+				<div class="whoWeAre-card-desc">
+					<div class="whoWeAre-card-header">
+						<h1>TechAnnex has been serving the <br> community since 2007</h1>
+					</div>
+					<div class="whoWeAre-card-text">
+						<p>
+							providing reliable and secure e-waste pickup services for <br> businesses and local communities. We offer a safe and legal <br> solution to dispose of unwanted electronics while ensuring the <br> protection of sensitive data.
+						</p>
+					</div>
+					<p class="readMoreHere">Read more here</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="ourImpact">
+			<div class="ourImpact-title">
+				<h1>Our Impact</h1>
+			</div>
+
+			<div class="ourImpact-cards-container">
+				<div class="ourImpact-card">
+					<img src={AboutUsAssets.ImpactOne} alt="" srcset="" class="" />
+				</div>
+				<div class="ourImpact-card">
+					<img src={AboutUsAssets.ImpactTwo} alt="" srcset="" class="" />
+				</div>
+			</div>
+		</div>
+
+		<div class="howWeOperate">
+			<div class="howWeOperate-title">
+				<h1>How We Operate</h1>
+			</div>
+
+			<div class="howWeOperate-container">
+				<div class="howWeOperate-desc">
+					<h1>Partnering with certified <br>recyclers to safely process <br>collected e-waste</h1>
+
+					<p>Collaborating with community <br>organizations for event locations</p>
+
+					<button type="submit">
+						<p>Be a Partner</p>
+						<span class="button-effect" id="mask"></span>
+						<span class="button-effect" id="bg"></span>
+					</button>
+				</div>
+				<div class="howWeOperate-img">
+					<img src={AboutUsAssets.Operate} alt="" srcset="" class="" />
+				</div>
+			</div>
+
+		</div>
+
+	</section>
 </main>
